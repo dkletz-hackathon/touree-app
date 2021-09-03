016b3d0429faf2d9056ca051192cb2406d69e412
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2021-09-04 06:59:07 +0700

    Add base store implementation

--- src/App.js
+++ src/App.js
@@ -3,29 +3,35 @@ import {
   Route,
   Switch
 } from'react-router-dom'
+import { Provider } from 'mobx-react'
 
+import RootStore from './stores'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/shards-dashboards.1.1.0.min.css'
 
 import routes from './routes'
 
 function App() {
+  const { ...otherStores } = RootStore
+
   return (
-    <Router>
-      <Switch>
-        {routes.map(route => {
-          return (
-            <Route
-              key={route.path}
-              path={route.path}
-              exact={route.exact}
-            >
-              <route.component routeChildren={route.children} />
-            </Route>
-          )
-        })}
-      </Switch>
-    </Router>
+    <Provider rootStore={RootStore} {...otherStores}>
+      <Router>
+        <Switch>
+          {routes.map(route => {
+            return (
+              <Route
+                key={route.path}
+                path={route.path}
+                exact={route.exact}
+              >
+                <route.component routeChildren={route.children} />
+              </Route>
+            )
+          })}
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
