8909054e2f5f160fa9870dac6cb86ec8fd27da31
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2021-09-05 04:22:20 +0700

    Add production API and feedback after publishing video

--- src/api/const.js
+++ src/api/const.js
@@ -1,3 +1,5 @@
-const BASE_URL = "http://68.183.224.203/api"
+const PROD_URL = `http://${window.location.hostname}/api`
+const INT_URL = "http://68.183.224.203/api"
+const BASE_URL = INT_URL
 
 export { BASE_URL }
