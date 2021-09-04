2515b4af16cbc8ec98333c3cb8b9005ebb069067
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2021-09-04 08:51:31 +0700

    Add sweetalert package and publish video modal for creator studio

---
+++ src/data/mockVideo.js
@@ -0,0 +1,7 @@
+const mockVideo = {
+  thumbnail: 'https://images.unsplash.com/photo-1544644181-1484b3fdfc62?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80',
+  title: 'Come! Join Us in a Virtual Trip to Bali',
+  desc: 'We will take a look around into Bali wonders, go grab a note cause you never know you might want to write up some of these places name',
+}
+
+export default mockVideo
--- src/pages/creator/studio/CreatorStudio.js
+++ src/pages/creator/studio/CreatorStudio.js
@@ -1,17 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { toJS } from 'mobx'
 import { inject, observer } from 'mobx-react'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 import NodeSettings from './NodeSettings'
 import ProjectSettings from './ProjectSettings'
 import StudioEditor from './StudioEditor'
 import './style.scss'
+import mockVideo from '../../../data/mockVideo'
 import logo from '../../../assets/logo-studio.png'
 import profile from '../../../assets/thumbnails/profile1.jpg'
 
+let Modal = withReactContent(Swal)
+
 const CreatorStudio = inject('nodeStore')(observer(
   function CreatorStudio(props) {
+    let history = useHistory()
     let [selectedNode, setSelectedNode] = React.useState(null)
 
     let showNodeSettings = id => {
@@ -22,6 +28,46 @@ const CreatorStudio = inject('nodeStore')(observer(
       setSelectedNode(null)
     }
 
+    let handlePublish = () => {
+      Modal.fire({
+        width: 600,
+        showConfirmButton: false,
+        html: (
+          <>
+            <p className="studio-modal-header">You will publish following video</p>
+            <div className="studio-modal">
+              <img src={mockVideo.thumbnail} alt="publish" />
+              <div className="studio-modal-info">
+                <h1>{mockVideo.title}</h1>
+                <p>{mockVideo.desc}</p>
+                <div className="studio-modal-info-icons">
+                  <span className="material-icons">account_tree</span>
+                  <span className="material-info">4 Video Paths</span>
+                  <span className="material-icons">query_builder</span>
+                  <span className="material-info">5 Minutes</span>
+                </div>
+              </div>
+            </div>
+            <div className="studio-modal-actions">
+              <button onClick={closeModal}>Cancel</button>
+              <button onClick={publishVideo}>Publish & Show</button>
+            </div>
+          </>
+        )
+      })
+    }
+
+    let closeModal = () => {
+      Modal.close()
+    }
+
+    let publishVideo = () => {
+      Modal.close()
+      setTimeout(() => {
+        history.push('/video/test-video')
+      }, 500)
+    }
+
     return (
       <>
         <div className="studio-navbar">
@@ -29,7 +75,7 @@ const CreatorStudio = inject('nodeStore')(observer(
             <img src={logo} alt="logo" className="studio-logo" />
           </Link>
           <div className="studio-navbar-actions">
-            <button onClick={null}>
+            <button onClick={handlePublish}>
               <span className="material-icons">movie</span>
               <p>Publish Video</p>
             </button>
